fc1cee2a546626164176d400d988f04651cfdc46
Yusuf Khan <146713368+itsuf@users.noreply.github.com>
2023-10-19 08:52:12 +0000

    committest

--- functions2.c
+++ functions2.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
-int sum (float a, float b)
+float sum (float a, float b)
 {
     float answer;
     answer = a + b;
     return answer;
 }
 
-int main ()
+float main ()
 {
     float y = 2.21;
     float z = sum(5.45, y); 
 
-    printf("The sum of 5.45 and %2.6f is %2.6f\n", &y, &z);
+    printf("The sum of 5.45 and %f is %f\n", y, z);
 }
